7c745548998a032e829e09e8ead9da4b4025197d
Uche Okoroafor <ucheokoroafor018@gmail.com>
2021-06-30 21:58:03 +0400

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,21 +1,21 @@
 <template>
 
     <body>
-        <div class="footer-dark" >
+        <div class="footer-dark">
             <footer>
                 <div class="container">
                     <div class="row">
-                        <div class="col-sm-6 col-md-3 item" >
-                        <h3>About</h3>
+                        <div class="col-sm-6 col-md-3 item">
+                            <h3>About</h3>
                             <ul>
-                                <li><a href="#">About Us</a></li>
-                                <li><a href="#">Contact Us</a></li>
+                                <li><a href="https://www.linkedin.com/in/uche-okoroafor-a9286b19b"   target="_blank">About Us</a></li>
+                                <li><a href="https://www.linkedin.com/in/uche-okoroafor-a9286b19b"   target="_blank">Contact Us</a></li>
                                 <li><a href="#">Help</a></li>
                             </ul>
                         </div>
                         <div class="col-sm-6 col-md-3 item">
-                          
-      <h3>Policies</h3>
+
+                            <h3>Policies</h3>
                             <ul>
                                 <li><a href="#">Terms & Conditions</a></li>
                                 <li><a href="#">Website Accessibility</a></li>
@@ -23,27 +23,27 @@
                             </ul>
                         </div>
                         <div class="col-md-6 item text">
-                            <h3>Company Name</h3>
+                            <h3>SoMedia</h3>
                             <p>Praesent sed lobortis mi. Suspendisse vel placerat ligula. Vivamus ac sem lacus. Ut
                                 vehicula rhoncus elementum. Etiam quis tristique lectus. Aliquam in arcu eget velit
                                 pulvinar dictum vel in justo.</p>
                         </div>
                         <div class="col item social" ref="footerOnScreen">
-<a href="#">
-            <font-awesome-icon :icon="{ prefix: 'fab', iconName: 'facebook-f' }" />
+                            <a href="https://m.facebook.com/uche.okoroafor" target="_blank">
+                                <font-awesome-icon :icon="{ prefix: 'fab', iconName: 'facebook-f' }" />
 
-                            </a><a href="#">
-            <font-awesome-icon :icon="{ prefix: 'fab', iconName: 'twitter' }" />
+                            </a><a href="https://twitter.com/Uchman101"   target="_blank" >
+                                <font-awesome-icon :icon="{ prefix: 'fab', iconName: 'twitter' }" />
 
-                            </a><a href="#">
-            <font-awesome-icon :icon="{ prefix: 'fab', iconName: 'github' }" />
+                            </a><a href="https://github.com/uche-okoroafor"   target="_blank" >
+                                <font-awesome-icon :icon="{ prefix: 'fab', iconName: 'github' }" />
 
-                            </a><a href="#">
-            <font-awesome-icon :icon="{ prefix: 'fab', iconName: 'instagram' }" />
+                            </a><a href="https://www.instagram.com/uche_okoroafor/"   target="_blank" >
+                                <font-awesome-icon :icon="{ prefix: 'fab', iconName: 'instagram' }" />
 
                             </a>
-                            <a href="#">
-            <font-awesome-icon :icon="{ prefix: 'fab', iconName: 'linkedin-in' }" />
+                            <a href="https://www.linkedin.com/in/uche-okoroafor-a9286b19b"   target="_blank" >
+                                <font-awesome-icon :icon="{ prefix: 'fab', iconName: 'linkedin-in' }" />
 
                             </a>
                         </div>
@@ -65,70 +65,72 @@
             }
         },
 
-  mounted() {
+        mounted() {
 
-      document.addEventListener('scroll', () => this.onScroll(this.$refs));
-    },
-    beforeUnmount() {
+            document.addEventListener('scroll', () => this.onScroll(this.$refs));
+        },
+        beforeUnmount() {
 
-      document.removeEventListener('scroll', () => this.onScroll(this.$refs));
-    },
+            document.removeEventListener('scroll', () => this.onScroll(this.$refs));
+        },
 
-methods: {
-    
-      onScroll(ref) {
+        methods: {
 
-
-
-            if (ref["footerOnScreen"] !== null) {
-
-
-if (this.isElementInViewport(ref["footerOnScreen"])) {
-
-  this.$store.dispatch("handleDisplayFunctions", {
-            footerOnScreen: true,
-            params: "footerOnScreen"
-          });
-
-
-    
-}else{
-
-  this.$store.dispatch("handleDisplayFunctions", {
-            footerOnScreen:false,
-            params: "footerOnScreen"
-          });
-
-}
+            onScroll(ref) {
 
 
 
+                if (ref["footerOnScreen"] !== null) {
 
-          }
+
+                    if (this.isElementInViewport(ref["footerOnScreen"])) {
+
+                        this.$store.dispatch("handleDisplayFunctions", {
+                            footerOnScreen: true,
+                            params: "footerOnScreen"
+                        });
+
+
+
+                    } else {
+
+                        this.$store.dispatch("handleDisplayFunctions", {
+                            footerOnScreen: false,
+                            params: "footerOnScreen"
+                        });
+
+                    }
 
 
 
 
-      },
+                }
 
-      isElementInViewport(el) {
-        var rect = el.getBoundingClientRect();
-        return (
-          rect.top >= 0 &&
-          rect.left >= 0 &&
-          rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-          rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-        );
-      },
-},
+
+
+
+            },
+
+            isElementInViewport(el) {
+                var rect = el.getBoundingClientRect();
+                return (
+                    rect.top >= 0 &&
+                    rect.left >= 0 &&
+                    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+                    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+                );
+            },
+        },
     }
 </script>
 <style>
     .footer-dark {
+box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.5);
         padding: 50px 0;
         color: #3aa1dd;
         background-color: #01022F;
-border-top: 2px solid #e83e8c;
+        border-top: 2px solid #e83e8c;
+
     }
 
     .footer-dark h3 {
@@ -153,7 +155,7 @@ border-top: 2px solid #e83e8c;
     }
 
     .footer-dark ul a:hover {
-      color:#e83e8c ;
+        color: #e83e8c;
     }
 
     @media (max-width:767px) {
@@ -179,12 +181,14 @@ border-top: 2px solid #e83e8c;
     }
 
     .footer-dark .item.social {
-        text-align: center;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-flow: row;
     }
 
     @media (max-width:991px) {
         .footer-dark .item.social {
-            text-align: center;
             margin-top: 20px;
         }
     }
@@ -195,19 +199,23 @@ border-top: 2px solid #e83e8c;
         width: 36px;
         height: 36px;
         line-height: 36px;
-        display: inline-block;
+        display: flex;
+        justify-content: center;
+        align-items: center;
         text-align: center;
         border-radius: 50%;
-        /* box-shadow: 0 0 0 1px rgba(255, 255, 255, 0.4); */
+        /* box-shadow: 0 0 0 1px rgba(58, 161, 221, 1); */
         margin: 0 8px;
-        color: #fff;
-        opacity: 0.75;
-border: 2px solid #e83e8c;
+        color: #3aa1dd;
+        border: 1.8px solid #3aa1dd;
+transition: all 0.5s;
 
     }
 
     .footer-dark .item.social>a:hover {
-        opacity: 0.9;
+        color: #e83e8c;
+        border: 1.8px solid #e83e8c;
+transition: all 0.5s;
     }
 
     .footer-dark .copyright {
@@ -215,6 +223,6 @@ border: 2px solid #e83e8c;
         padding-top: 24px;
         font-size: 13px;
         margin-bottom: 0;
-color:#e83e8c  ;
+        color: #e83e8c;
     }
 </style>
