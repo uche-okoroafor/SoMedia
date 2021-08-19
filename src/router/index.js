539ddbe23965c1ce28693a915fdefc698daeacfe
Uche Okoroafor <ucheokoroafor018@gmail.com>
2021-08-19 12:38:18 +0400

    Add files via upload

--- src/router/index.js
+++ src/router/index.js
@@ -1,57 +1,75 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
 import  PageNotFound from '../views/PageNotFound.vue'
+import  Home from '../views/Home.vue'
+import  Timeline from '../views/Timeline.vue'
 import UserProfile from '../views/UserProfile.vue'
-import Timeline from '../views/Timeline.vue'
-import LogInPage from '../views/LogInPage.vue'
-import Messages from '../views/Messages.vue'
-import Friends from '../views/Friends.vue'
+
 
 const routes = [
+{
+    path: '/login',
+    name: 'Login',
+  component: () => import(/* webpackChunkName: "LogIn" */ '../views/LogInPage.vue')
+  },
+
+ {
+    path: '/',
+    name: 'Login',
+  component: () => import(/* webpackChunkName: "Login" */ '../views/LogInPage.vue')
+
+  },
+
+ 
+
+
 
  {
     path: '/Home',
     name: 'Home',
-    component: Home,
+  component:Home
   },
- 
- {
-      path: '/:catchAll(.*)',
-      name: PageNotFound,
-      component: PageNotFound
-    },
 
+
+
+ {
+    path: '/Newsfeed',
+    name: 'Newsfeed',
+  component: () => import(/* webpackChunkName: "Home" */ '../views/Home.vue')
+
+  },
   {
-    path: '/:userName/:Timeline',
+    path: '/:userName/Timeline',
     name: 'Timeline',
-    component:Timeline,
-      props:true
+  component:Timeline,
+   props:true
   },
- {
-    path: '/login',
-    name: 'Login',
-    component:LogInPage,
 
-  },
  {
     path: '/Home/:userName',
     name: 'userProfile',
-    component:UserProfile,
+  component:UserProfile, 
 props:true
   },
  {
     path: '/:userName/messages',
     name: 'Messages',
-    component:Messages,
+  component: () => import(/* webpackChunkName: "Messages" */ '../views/Messages.vue'),
 props:true
   },
 
  {
     path: '/:userName/friends',
     name: 'Friends',
-    component:Friends,
+  component: () => import(/* webpackChunkName: "Friends" */ '../views/Friends.vue'),
+
 props:true
   },
+
+ {
+      path:'/:catchAll(.*)',
+      name: PageNotFound,
+      component: PageNotFound
+    },
 
 ]
 
